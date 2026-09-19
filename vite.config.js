c479cb2ddd994af40ed834550d674417243cd858
ChathuAbeyrathna <abeyrathnamrcd.21@uom.lk>
2026-09-19 21:26:24 +0530

    new build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
+
+  base: command === 'build' ? '/Aurelia-Grand/' : '/',
+
   server: {
     port: 5173
   }
-})
+}))
